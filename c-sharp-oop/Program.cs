969d4ca554f35f8b53c9d6983036416e2d1e2d90
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-14 21:32:22 -0500

    Add my first class

--- c-sharp-oop/Program.cs
+++ c-sharp-oop/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+Monitor miMonitor = new Monitor(); // hay que instanciar la clase antes de la creación de la misma
+var CarlosMonitor = new Monitor();
+
+miMonitor.brand = "Sceptre";
+miMonitor.color = "Black";
+miMonitor.size = 27;
+miMonitor.buttonCount = 5;
+miMonitor.hasSpeakers = true;
+miMonitor.herz = 165;
+
+CarlosMonitor.brand = "Samsung";
+CarlosMonitor.color = "White";
+CarlosMonitor.size = 17;
+CarlosMonitor.buttonCount = 3;
+CarlosMonitor.hasSpeakers = false;
+CarlosMonitor.herz = 90;
+
+class Monitor
+{
+  public string brand; // si no se usa ningun access modifier, por defecto es private
+  public string color;
+  public int size;
+  public short buttonCount; // short -> es un tipo de dato numérico que almacena numeros pequeños
+  public bool hasSpeakers;
+  public ushort herz;
+}
